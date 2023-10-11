053fb5e9f8dd41c6ad655187c61a755f5db425a4
valevalencia <valentinavalenciacarvajal16@gmail.com>
2023-10-10 22:43:40 -0500

    build: env changes
    env changes

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build' // Configura la carpeta de salida como 'build'
-  }
 })
-
-
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
